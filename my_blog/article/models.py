d991dea605642f2d31a1eadbbe58434d32a263be
lv <1036528154@qq.com>
2017-05-23 22:31:41 +0800

    my_bolg show chinese and test for news

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
 
 class Article(models.Model):
     title = models.CharField(max_length=100) # 博客题目
---
+++ news/news/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+class BaiduNewsItem(scrapy.Item):
+    title = scrapy.Field()      #新闻的标题
+    content = scrapy.Field()    #新闻的大概内容
+    from_where = scrapy.Field() #新闻的出处
+    url = scrapy.Field()        #新闻的链接
+
