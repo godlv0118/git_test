a101be9397e0a224bc3066e97dfc2ad973332be6
lv <1036528154@qq.com>
2017-05-23 23:31:38 +0800

    just finish models and finish db connect

--- NewPage/Baidu/models.py
+++ NewPage/Baidu/models.py
@@ -3,8 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
 # Create your models here.
 
 class Baidu(models.Model):
-    title = models.Field
+    title = models.CharField(max_length=100)      #新闻的标题
+    content = models.TextField(max_length=200)    #新闻的大概内容
+    from_where = models.CharField(max_length=20,blank=True) #新闻的出处
+    url = models.CharField(max_length=100)        #新闻的链接
+
+    def __str__(self):
+        return self.title
