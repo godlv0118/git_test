7548c3ad3d76a75b16813a33d53313d3df01d720
lv <1036528154@qq.com>
2017-05-31 12:56:07 +0800

    改正不能parse

--- news/news/spiders/Spider.py
+++ news/news/spiders/Spider.py
@@ -1,14 +1,17 @@
 #-*- coding: utf-8 -*-
 import sys
+import scrapy
+from scrapy.http import Request
 from scrapy.spider import Spider
 from scrapy.selector import Selector
-from news.items import BaiduNewsItem
+from ..items import BaiduNewsItem
 class BaiduSpider(Spider):
     name = 'baidu'
     allowed_domains = ['news.baidu.com']
 
     start_urls = []
-
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+    headers = {'User-Agent': user_agent}
     def start_requests(self):
 
         number = 20
@@ -19,8 +22,10 @@ class BaiduSpider(Spider):
 
         for url in self.start_urls:
             yield self.make_requests_from_url(url)
+            #yield scrapy.Request(url,callback=self.parse,headers=self.headers)
 
     def parse(self, response):
+        print response.body
         html = Selector(response)
         item = BaiduNewsItem()
         data = html.xpath('//div')
